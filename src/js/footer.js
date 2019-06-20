65d506e70164cef62a3d7e90e9134039bc8fae1b
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-06-20 15:47:06 -0700

    footer

---
+++ src/js/footer.js
@@ -0,0 +1,35 @@
+let path = document.querySelector('#footer path') ;
+let w = window.innerWidth ;
+let h = document.querySelector('#footer').clientHeight ;
+let params = {
+    curveControlX: w/2,
+    curveControlY: 2*h/3,
+    curvefinalX: w,
+    curvefinalY: 0,
+    line1X: 0,
+    line1Y: h,
+    line2X: w,
+    line2Y: 0,
+}
+let shape = `M0,0 q${params.curveControlX},${params.curveControlY} 
+    ${params.curvefinalX},${params.curvefinalY} 
+    l${params.line1X},${params.line1Y} 
+    l${-params.line2X},${params.line2Y} z` ;
+path.setAttribute('d',shape) ;
+window.addEventListener('resize',e=>{
+    w = window.innerWidth ;
+    h = document.querySelector('#footer').clientHeight ;
+    params.curveControlX = w/2 ;
+    params.curveControlY = 2*h/3 ;
+    params.curvefinalX = w ;
+    params.curvefinalY = 0 ;
+    params.line1X = 0 ;
+    params.line1Y = h ;
+    params.line2X = w ;
+    params.line2Y = 0 ;
+    shape = `M0,0 q${params.curveControlX},${params.curveControlY} 
+        ${params.curvefinalX},${params.curvefinalY} 
+        l${params.line1X},${params.line1Y} 
+        l${-params.line2X},${params.line2Y} z` ;
+    path.setAttribute('d',shape) ;
+});
--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,10 @@ import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css' ;
 import '../style/init.scss' ;
 import '../style/parallax.scss' ;
 import '../style/services.scss' ;
+import '../style/suggestions.scss' ;
+import '../style/features.scss' ;
+import '../style/footer.scss' ;
 //load scripts
 import './parallax' ;
 import './grid-animation' ;
+import './footer' ;
