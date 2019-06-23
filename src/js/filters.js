55ea8e1ce4afecb79f2301025a6d133740a23f55
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-06-24 01:07:39 +0330

    ranger v2

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,40 +1,39 @@
-import noUiSlider from 'materialize-css/extras/noUiSlider/nouislider' ;
+import wNumb from 'wnumb' ;
+import noUiSlider from 'nouislider' ;
 let range = document.querySelector('#range');
 noUiSlider.create(range, {
-	range: {
-        'min': 1300,
-        'max': 3250
-    },
-
-    step: 150,
-
-    // Handles start at ...
-    start: [1450, 2050, 2350, 3000],
-
-    // ... must be at least 300 apart
-    margin: 300,
-
-    // ... but no more than 600
-    limit: 600,
-
-    // Display colored bars between handles
-    connect: true,
-
-    // Put '0' at the bottom of the slider
+    orientation: 'horizontal' ,
     direction: 'rtl',
-    orientation: 'horizontal',
-
-    // Move handle on tap, bars are draggable
-    behaviour: 'tap-drag',
-    tooltips: true,
-    format: wNumb({
-        decimals: 0
-    }),
-
-    // Show a scale with the slider
-    pips: {
-        mode: 'steps',
-        stepped: true,
-        density: 4
-    }
+    animate: true,
+    animationDuration: 300,
+    behaviour: 'tap', 
+    range: {
+        'min': [0],
+        'max': [2000000]
+    },
+    start: [0,2000000],
+    connect: [false,true,false],
+    tooltips: [
+        wNumb({
+            decimals: 0, 
+            thousand: '.', 
+            suffix: ' تومان' 
+        }) ,
+        wNumb({
+            decimals: 0, 
+            thousand: '.', 
+            suffix: ' تومان' 
+        })      
+    ] ,
+    format: wNumb({ 
+        decimals: 0, 
+        thousand: '.', 
+        suffix: ' تومان'
+    })
 });
+let elm = document.querySelectorAll('.noUi-handle') ;
+console.log(elm) ;
+elm.forEach(e=>{
+    e.style.backgroundColor = 'red' ;
+    e.classList.add('newC') ;
+})
--- src/js/hotels.js
+++ src/js/hotels.js
@@ -1,14 +1,13 @@
 //load styles
 import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css' ;
 import '../style/init.scss' ;
-import '../../node_modules/materialize-css/extras/noUiSlider/nouislider.css' ;
+import '../../node_modules/nouislider/distribute/nouislider.min.css' ;
 import './../../node_modules/materialize-css/dist/css/materialize.min.css' ;
 import '../style/slider.scss' ;
 import '../style/search.scss' ;
 import '../style/filters.scss' ;
 //import '../style/footer.scss' ;
 //load scripts
-// import '../../node_modules/materialize-css/extras/noUiSlider/nouislider.min.js' ;
 import 'materialize-css' ;
 import './slider' ;
 import './search' ;
