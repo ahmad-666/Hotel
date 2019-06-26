345bdafd0194e65860f7f68d16f9bdec75980b22
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-06-26 14:04:57 -0700

    tabs

--- src/js/hotel.js
+++ src/js/hotel.js
@@ -4,9 +4,11 @@ import '../style/material-icons.css' ;
 import '../style/init.scss' ;
 import './../../node_modules/materialize-css/dist/css/materialize.min.css' ;
 import '../style/hotel-info.scss' ;
+import '../style/tabs.scss' ;
 //import '../style/footer.scss' ;
 //import '../style/responsive.scss' ;
 //load scripts
 import 'materialize-css' ;
 import './hotel-info' ;
+import './tabs' ;
 //import './footer' ;
---
+++ src/js/tabs.js
@@ -0,0 +1,6 @@
+
+let elem = document.querySelector('.tabs');
+let instances = M.Tabs.init(elem);
+//indicator style------------
+let indicator = document.querySelector('.tabs .indicator');
+indicator.style.backgroundColor = 'teal' ;
