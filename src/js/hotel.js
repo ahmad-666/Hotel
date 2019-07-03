b9a49065363e911a0913f1f62bdfbdfec4168942
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-07-03 20:29:10 +0330

    scode,date,flip fix

--- src/js/hotel.js
+++ src/js/hotel.js
@@ -4,6 +4,7 @@ import '../style/material-icons.css' ;
 import '../style/init.scss' ;
 import './../../node_modules/materialize-css/dist/css/materialize.min.css' ;
 import '../style/hotel-info.scss' ;
+import '../style/search.scss' ;
 import '../style/tabs.scss' ;
 import '../style/reserve.scss' ;
 import '../style/footer.scss' ;
@@ -11,6 +12,7 @@ import '../style/responsive.scss' ;
 //load scripts
 import 'materialize-css' ;
 import './hotel-info' ;
+import './search' ;
 import './tabs' ;
 import './reserve' ;
 import './footer' ;
