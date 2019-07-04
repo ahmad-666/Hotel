6ce2f0de32a315a4772c85fa58df02435fedad83
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-07-04 14:04:28 -0700

    change reserve table to card

--- src/js/reserve.js
+++ src/js/reserve.js
@@ -1,62 +1,31 @@
-//guest numbers toolTip-----------------------------
-//--------------------------------------------------
-//--------------------------------------------------
-let guestNumTooltip = document.querySelectorAll('#reserve .guests-num.tooltipped') ;
-let guestNum = 4 ;
-let guestToolTipConfig = {
-    position: 'top',
-    margin: -50
-}
-guestNumTooltip.forEach(tooltip=>{
-    let guestNum = 4 ;
-    M.Tooltip.init(tooltip,{
-        ...guestToolTipConfig ,
-        html: `<span> ${guestNum} نفر </span>`
-    })
+//Increase/Decrease room-num
+//--------------------------
+//--------------------------
+let increaseBtn = document.querySelector('.room button#increase') ;
+let decreaseBtn = document.querySelector('.room button#decrease') ;
+let roomNum = document.querySelector('.room .room-num input[type="number"]') ;
+increaseBtn.addEventListener('click',e=>{
+    roomNum.value++ ;
 })
-//features toolTip----------------------------------
-//--------------------------------------------------
-//--------------------------------------------------
-let featuresTooltip = document.querySelectorAll('#reserve .features .tooltipped') ;
-let wifiTooltipHTML = "<span class='white-text'> وای فای </span>" ;
-let tvTooltipHTML = "<span class='white-text'> تلویزیون </span>" ;
-let dumbbellTooltipHTML = "<span class='white-text'> باشگاه </span>" ;
-let poolTooltipHTML = "<span class='white-text'> استخر </span>" ;
-let showerTooltipHTML = "<span class='white-text'> حمام </span>" ;
-let resturantTooltipHTML = "<span class='white-text'> رستوران </span>" ;
-let featureTooltipConfig = {
-    position: 'top' ,
-    margin: 0
-}
-featuresTooltip.forEach(tooltip=>{
-    if([...tooltip.classList].join().includes('wifi')) {
-        M.Tooltip.init(tooltip,{
-            ...featureTooltipConfig,
-            html: wifiTooltipHTML
-        })
-    }
-    else if([...tooltip.classList].join().includes('tv')) {
-        M.Tooltip.init(tooltip,{
-            ...featureTooltipConfig,
-            html: tvTooltipHTML
-        })
-    }
-    else if([...tooltip.classList].join().includes('dumbbell')) {
-        M.Tooltip.init(tooltip,{
-            ...featureTooltipConfig,
-            html: dumbbellTooltipHTML
-        })
-    }
-    else if([...tooltip.classList].join().includes('shower')) {
-        M.Tooltip.init(tooltip,{
-            ...featureTooltipConfig,
-            html: showerTooltipHTML 
-        })
-    }
-    else if([...tooltip.classList].join().includes('pool')) {
-        M.Tooltip.init(tooltip,{
-            ...featureTooltipConfig,
-            html: poolTooltipHTML
-        })
-    }   
+decreaseBtn.addEventListener('click',e=>{
+    (roomNum.value-1>=0)?(roomNum.value--):(roomNum.value) ;
 })
+//features tooltip
+//--------------------------
+//--------------------------
+let tooltips = document.querySelectorAll('#rooms .features .tooltipped') ;
+let tooltipConfig = {
+    position: 'top' 
+}
+let wifiTooltip = 'وای فای' ;
+tooltips.forEach(tooltip=>{
+    if([...tooltip.classList].join().includes('wifi')){
+        M.Tooltip.init(tooltip,{
+            ...tooltipConfig ,
+            html: `<span class="white-text">${wifiTooltip}</span>`
+        })
+    }
+})
+//Open calender grid
+//--------------------------
+//--------------------------
