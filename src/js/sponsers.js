558a7a10bb658a94e7a4ff75f6ff9b054feac261
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-07-10 22:15:52 +0330

    resp v1.2

--- src/js/sponsers.js
+++ src/js/sponsers.js
@@ -1,13 +1,20 @@
 import anime from 'animejs' ;
 let slider = document.querySelector('#sponsers .my-slider') ;
-let offset = Math.abs(window.innerWidth - slider.clientWidth) + slider.querySelector('.my-slide').clientWidth ;
-let time = (slider.childElementCount)*2*1000 ;
-anime({
-    targets: slider ,
-    duration: time ,
-    easing: 'linear' ,
-    delay: 0 ,
-    direction: 'alternate' ,
-    loop: true ,
-    right: -offset 
-})
+let slide = slider.querySelector('.my-slide') ;
+let totalSliderWidth = (parseInt(window.getComputedStyle(slide,null).getPropertyValue('margin-right')) + 
+            parseInt(window.getComputedStyle(slide,null).getPropertyValue('margin-left')) +
+            slide.clientWidth) * slider.childElementCount ;
+if(totalSliderWidth > window.innerWidth) {
+    let time = (slider.childElementCount)*2*1000 ;
+    let offset = Math.abs(totalSliderWidth - window.innerWidth); 
+    anime({
+        targets: slider ,
+        duration: time ,
+        easing: 'linear' ,
+        delay: 0 ,
+        direction: 'alternate' ,
+        loop: true ,
+        right: -offset 
+    })
+}
+
--- src/js/suggestions.js
+++ src/js/suggestions.js
@@ -1,95 +1,37 @@
-let prevBtns = document.querySelectorAll('.slider-wrapper .btn.btn-right') ;
-let nextBtns = document.querySelectorAll('.slider-wrapper .btn.btn-left') ;
-let slider = null;
-let offset = null ;
-let currPos = 0 ;
-prevBtns.forEach(btn=>btn.addEventListener('click',prevHadndler)) ;
-nextBtns.forEach(btn=>btn.addEventListener('click',nextHadndler)) ;
-function nextHadndler(e){
-    slider = this.parentElement.querySelector('.slider');
-    offset = slider.querySelector('.slide').clientWidth ;
-    if(Math.abs(currPos-offset) <= offset*slider.childElementCount){
-        currPos-=offset ;
-        slider.style.right = `${currPos}px` ;
-    } 
+function Slider(id){
+    this.id = id ;
+    this.sliderWrapper = document.querySelector(id) ;
+    this.slider = this.sliderWrapper.querySelector('.slider') ;
+    this.slide = this.slider.querySelector('.slide') ;
+    this.prevBtn = this.sliderWrapper.querySelector('.btn-right') ;
+    this.nextBtn = this.sliderWrapper.querySelector('.btn-left') ;
+    this.curPos = 0 ;
+    this.offset = null ;
+    this.visibleSlidesNum = null ; //number of visible slides in viewport
+    this.threshold = null ;
 }
-function prevHadndler(e){
-    slider = this.parentElement.querySelector('.slider');
-    offset = slider.querySelector('.slide').clientWidth ;
-    if(currPos+offset < offset){
-        currPos+=offset ;
-        slider.style.right = `${currPos}px` ;
-    } 
+Slider.prototype.prevBtnInit = function(){
+    this.prevBtn.addEventListener('click',e=>{
+        this.offset = (this.slide.clientWidth) + parseInt(window.getComputedStyle(this.slide,null).getPropertyValue('margin-right')) + parseInt(window.getComputedStyle(this.slide,null).getPropertyValue('margin-left')) ;
+        this.visibleSlidesNum = Math.round(window.innerWidth / this.offset) ;
+        this.threshold = 0 ;
+        if(this.curPos+this.offset <= this.threshold ) this.slider.style.right = `${this.curPos+=this.offset}px` ;
+    });
 }
-// let offset = document.querySelector('.slider-wrapper .slide').clientWidth ;
-// let discountSlider = document.querySelector('#discount .slider') ;
-// let discountRight = 0 ;
-// let discountThreshold = Math.abs(window.innerWidth - discountSlider.clientWidth)+offset ;
-// let topSlider = document.querySelector('#top .slider') ;
-// let topRight = 0 ;
-// let topThreshold = Math.abs(window.innerWidth - topSlider.clientWidth)+offset ;
-// let suggestionSlider = document.querySelector('#suggestion .slider') ;
-// let suggestionRight = 0 ;
-// let suggestionThreshold = Math.abs(window.innerWidth - suggestionSlider.clientWidth)+offset ;
-// let prevBtns = document.querySelectorAll('.slider-wrapper .btn-right') ;
-// let nextBtns = document.querySelectorAll('.slider-wrapper .btn-left') ;
-// nextBtns.forEach(next=>next.addEventListener('click',nextAnimation));
-// prevBtns.forEach(prev=>prev.addEventListener('click',prevAnimation));
-// function nextAnimation(e){
-//     let next = this ;
-//     switch(next.parentNode){
-//         case discountSlider.parentNode:
-//             if(Math.abs(discountRight - offset) < discountThreshold){
-//                 discountRight -= offset
-//                 discountSlider.style.right = `${discountRight}px` ;
-//             }        
-//             break ;
-//         case topSlider.parentNode:
-//             if(Math.abs(topRight - offset) < topThreshold){
-//                 topRight -= offset
-//                 topSlider.style.right = `${topRight}px` ;
-//             }        
-//             break ;
-//         case suggestionSlider.parentNode:
-//             if(Math.abs(suggestionRight - offset) < suggestionThreshold){
-//                 suggestionRight -= offset
-//                 suggestionSlider.style.right = `${suggestionRight}px` ;
-//             }        
-//             break ;
-//     }
-// }
-// function prevAnimation(e){
-//     let prev = this ;
-//     switch(prev.parentNode){
-//         case discountSlider.parentNode:
-//             if(discountRight + offset <= 0){
-//                 discountRight += offset
-//                 discountSlider.style.right = `${discountRight}px` ;
-//             }        
-//             break ;
-//         case topSlider.parentNode:
-//             if(topRight + offset <= 0){
-//                 topRight += offset
-//                 topSlider.style.right = `${topRight}px` ;
-//             }        
-//             break ;
-//         case suggestionSlider.parentNode:
-//             if(suggestionRight + offset <= 0){
-//                 suggestionRight += offset
-//                 suggestionSlider.style.right = `${suggestionRight}px` ;
-//             }        
-//             break ;
-//     }
-// }
-// window.addEventListener('resize',e=>{
-//     discountSlider.style.right = 0 ;
-//     topSlider.style.right = 0 ;
-//     suggestionSlider.style.right = 0 ;
-//     discountRight = 0 ;
-//     topRight = 0 ;
-//     suggestionRight = 0 ;
-//     offset = document.querySelector('.slider-wrapper .slide').clientWidth ;
-//     discountThreshold = Math.abs(window.innerWidth - discountSlider.clientWidth)+offset ;
-//     topThreshold = Math.abs(window.innerWidth - topSlider.clientWidth)+offset ;
-//     suggestionThreshold = Math.abs(window.innerWidth - suggestionSlider.clientWidth)+offset ;
-// })
+Slider.prototype.nextBtnInit = function(){
+    this.nextBtn.addEventListener('click',e=>{
+        this.offset = (this.slide.clientWidth) + parseInt(window.getComputedStyle(this.slide,null).getPropertyValue('margin-right')) + parseInt(window.getComputedStyle(this.slide,null).getPropertyValue('margin-left')) ;
+        this.visibleSlidesNum = Math.round(window.innerWidth / this.offset) ;
+        this.threshold = (this.slider.childElementCount - this.visibleSlidesNum + 1)*this.offset ;
+        if(Math.abs(this.curPos-this.offset) <= this.threshold) this.slider.style.right = `${this.curPos-=this.offset}px` ;
+    });
+}
+let discountSlider = new Slider('#discount') ;
+discountSlider.prevBtnInit() ;
+discountSlider.nextBtnInit() ;
+let topSlider = new Slider('#top') ;
+topSlider.prevBtnInit() ;
+topSlider.nextBtnInit() ;
+let suggestiontSlider = new Slider('#suggestion') ;
+suggestiontSlider.prevBtnInit() ;
+suggestiontSlider.nextBtnInit() ;
