1edf40a8f2fee51d5d338dfff418e45b44d0173b
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-07-12 13:55:06 +0330

    half resp page 3

--- src/js/hotel-info.js
+++ src/js/hotel-info.js
@@ -1,52 +1,16 @@
 //-------------Tooltip----------------------------
 //------------------------------------------------
-let tooltips = document.querySelectorAll('#hotel-features .tooltipped');
-let wifiTooltipHTML = "<span class='white-text'> وای فای </span>" ;
-let tvTooltipHTML = "<span class='white-text'> تلویزیون </span>" ;
-let dumbbellTooltipHTML = "<span class='white-text'> باشگاه </span>" ;
-let poolTooltipHTML = "<span class='white-text'> استخر </span>" ;
-let showerTooltipHTML = "<span class='white-text'> حمام </span>" ;
-let resturantTooltipHTML = "<span class='white-text'> رستوران </span>" ;
-let initConfig = {
-    position: 'top' 
-}
-tooltips.forEach(elm=>{
-    if([...elm.classList].join().includes('wifi')) {
-        M.Tooltip.init(elm,{
-            ...initConfig,
-            html: wifiTooltipHTML
-        })
-    }
-    else if([...elm.classList].join().includes('tv')) {
-        M.Tooltip.init(elm,{
-            ...initConfig,
-            html: tvTooltipHTML
-        })
-    }
-    else if([...elm.classList].join().includes('dumbbell')) {
-        M.Tooltip.init(elm,{
-            ...initConfig,
-            html: dumbbellTooltipHTML
-        })
-    }
-    else if([...elm.classList].join().includes('pool')) {
-        M.Tooltip.init(elm,{
-            ...initConfig,
-            html: poolTooltipHTML
-        })
-    }
-    else if([...elm.classList].join().includes('shower')) {
-        M.Tooltip.init(elm,{
-            ...initConfig,
-            html: showerTooltipHTML
-        })
-    }
-    else if([...elm.classList].join().includes('utensils')) {
-        M.Tooltip.init(elm,{
-            ...initConfig,
-            html: resturantTooltipHTML
-        })
-    }
+let tooltips = document.querySelectorAll('#hotel-info .tooltipped') ;
+let tooltipConfig = {
+    position: 'top'
+};
+tooltips.forEach(tooltip=>{
+    let text = tooltip.getAttribute('data-text') ;
+    let html = `<span class="white-text"> ${text} </span>` ;
+    M.Tooltip.init(tooltip,{
+        ...tooltipConfig ,
+        html
+    })
 })
 //-------------Slider----------------------------
 //-----------------------------------------------
