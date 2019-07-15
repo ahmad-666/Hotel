7e3bbe5bddb2130635b7551573e56965e7537f3e
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-07-15 14:05:42 -0700

    rewritre filters

--- src/js/hotels.js
+++ src/js/hotels.js
@@ -1,12 +1,12 @@
 //load styles
 import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css' ;
-import '../style/material-icons.css' ;
+// import '../style/material-icons.css' ;
 import '../style/init.scss' ;
 import '../../node_modules/nouislider/distribute/nouislider.min.css' ;
-import './../../node_modules/materialize-css/dist/css/materialize.min.css' ;
+// import './../../node_modules/materialize-css/dist/css/materialize.min.css' ;
 import '../style/header.scss' ;
 import '../style/header-grid.scss' ;
-import '../style/slider.scss' ;
+//import '../style/slider.scss' ;
 import '../style/search.scss' ;
 import '../style/calender.scss' ;
 import '../style/grid-filters.scss' ;
@@ -19,16 +19,16 @@ import '../style/footer.scss' ;
 import '../style/copyright.scss' ;
 import '../style/modals.scss' ;
 //load scripts
-import 'materialize-css' ;
+// import 'materialize-css' ;
 import './header' ;
 import './header-grid' ;
-import './slider' ;
+//import './slider' ;
 import './search' ;
-import './calender' ;
-import './filters' ;
-import './sort' ;
-import './curr-filters' ;
-import './hotels-container' ;
-import './mobile' ;
+//import './calender' ;
+//import './filters' ;
+//import './sort' ;
+//import './curr-filters' ;
+//import './hotels-container' ;
+//import './mobile' ;
 //import './footer' ;
-import './modals'
+//import './modals'
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '@babel/polyfill';
 //load styles
 import '../../node_modules/@fortawesome/fontawesome-free/css/all.min.css' ;
 import '../style/material-icons.css' ;
-import './../../node_modules/materialize-css/dist/css/materialize.min.css' ;
+// import './../../node_modules/materialize-css/dist/css/materialize.min.css' ;
 import '../style/init.scss' ;
 import '../style/header.scss' ;
 import '../style/header-grid.scss' ;
@@ -18,13 +18,13 @@ import '../style/copyright.scss' ;
 import '../style/modals.scss' ;
 
 //load scripts
-import 'materialize-css'
+// import 'materialize-css'
 import './header' ;
 import './header-grid' ;
 import './search' ;
-import './calender' ;
-import './services' ;
-import './suggestions' ;
-import './sponsers' ;
-//import './footer' ;
-import './modals'
+// import './calender' ;
+// import './services' ;
+// import './suggestions' ;
+// import './sponsers' ;
+// //import './footer' ;
+// import './modals'
