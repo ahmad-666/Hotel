ae667e220ac96d88a0d812f7ea0a8151b1e7965d
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-07-18 12:30:32 -0700

    active tab

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -8,6 +8,10 @@ tabsTrigger.forEach(trigger=>{
 })
 function tabTriggerHandler(e){
     let targetTab = tabs.querySelector(`#content #${this.getAttribute('data-tab')}`) ;
+    tabsTrigger.forEach(tab=>{
+        tab.classList.remove('active') ;
+    })
+    this.classList.add('active') ;
     tabsContent.forEach(content => {
         content.classList.remove('show') ;
         content.style.display="none" ;
